af42a4648bb3b84e623e4bd0299ce836cf147359
AnnaRabychina <nutochka@tut.by>
2021-09-06 22:55:37 +0300

    complete task

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const GuessingGame = require('./src/guessing-game.js');
 
+class GuessingGame {
+    constructor(){
+     
+    }
+
+    setRange(min, max){
+        this.min = min;
+        this.max = max;
+    }
+
+    guess(){
+        this.number = Math.round((this.min + this.max) / 2)
+        return this.number
+    }
+    
+    lower(){
+          this.max = this.number - 1;
+    }
+
+    greater(){
+      this.min = this.number + 1;
+    }
+}
+
 window.game = new GuessingGame();
